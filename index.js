c2e71f0e6f68a3fea61af2eff14160b15bf50abe
thindHarminder <121890692+thindHarminder@users.noreply.github.com>
2023-02-20 19:10:16 -0800

    no Random spells

--- index.js
+++ index.js
@@ -18,19 +18,9 @@ $('#spells').on('click', async () => {
 });
 
 
-const randomSpell = () => {
-  const spells = ['Text Empty 😫', 'Im not ready yet 😡', 'Try Again with Text 😁']
-  const levels = ['I','II','III','IV','V']
-  return `${spells[getRandomInt(0, spells.length-1)]} ${levels[getRandomInt(0, levels.length-1)]}`
-}
-
-const randomAction = () => {
-  const actions = ['Saved', 'Added to cart', 'In cart', 'User removed', 'Timer set', 'Added to Favorites', `Multi-Line \n Support 👍`, 'This is just really long and completely unnecessary']
-  return `${actions[getRandomInt(0, actions.length-1)]}`
-}
-
 const getRandomInt = (min, max) => {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
